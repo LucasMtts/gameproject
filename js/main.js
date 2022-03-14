55cc99b8fa9607b70d7f5f0c53e678c60ca22e1a
Lucas <lucasgrcayres@gmail.com>
2022-03-14 16:48:42 -0300

    add kill and reset

--- js/main.js
+++ js/main.js
@@ -1,12 +1,34 @@
 // INITIAL DATA
-
+let pointView = document.getElementById('points');
+let lifesView = document.getElementById('lifes');
+let life = 3;
+let point = 0;
 
 // EVENTS
-document.getElementById("enemy").addEventListener("click", kill); 
+document.getElementById("enemy1").addEventListener("click", kill); 
+document.getElementById("reset").addEventListener('click', resetGame);
+document.getElementById("start").addEventListener('click', startGame);
 
 // FUNCTION
-function kill (){
-    
+function startGame(){
+   attScore();
+   document.getElementById("enemy1").style.display = "flex"
 }
 
+function kill (){
+    point++;
+   attScore();
+    
+    document.getElementById("enemy1").style.display = "none";
+}
 
+function resetGame() {
+    life = 3;
+    point = 0;
+    attScore();
+}
+
+function attScore() {
+    pointView.innerText = `Points: ${point}`;
+    lifesView.innerText = `Lifes: ${life}`;
+}
